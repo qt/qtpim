qt/qtpim

commit 5d9b3ea0bbbb9940cf9b6a4c6c11c6dce403cc48

Remove a memory leak from qcontactjsondbenginedata.
Change-Id: Ia2b81cc3afe718fff46277ddecc2c55134386da4
Sanity-Review: Qt Sanity Bot <qt_sanity_bot@ovi.com>
Reviewed-by: Claudio Brunelli <claudio.brunelli@nokia.com>

src/plugins/contacts/jsondb/qcontactjsondbenginedata.h
@@ -74,7 +74,9 @@ public:
 
     ~QContactJsonDbEngineData()
     {
-      // TODO
+        if (m_requestHandler)
+            delete m_requestHandler;
+        m_requestHandler = 0;
     }
 
     QContactJsonDbRequestHandler* m_requestHandler;

commit 8f941f6aafc3f73b0f6b73c27281312dd3487c85

Don't immediately delete requesthandler.
This change fixes a problem introduced by 5d9b3ea0bbbb9940cf9b6a4c6c11c6dce403cc48.
The handler object lives in another thread, and hence it should be disposed by using the deleteLater method, so as to avoid deleting the object while the thread is still running.

Change-Id: If3d8de821ecabe816c2d20d79c775f49bc5050f7
Sanity-Review: Qt Sanity Bot <qt_sanity_bot@ovi.com>
Reviewed-by: Claudio Brunelli <claudio.brunelli@nokia.com>

src/plugins/contacts/jsondb/qcontactjsondbenginedata.h
@@ -75,8 +75,7 @@ public:
     ~QContactJsonDbEngineData()
     {
         if (m_requestHandler)
-            delete m_requestHandler;
-        m_requestHandler = 0;
+            m_requestHandler->deleteLater();
     }
 
     QContactJsonDbRequestHandler* m_requestHandler;
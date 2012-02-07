qt/qtpim

commit 3a359d450fd927b58a4172a71a57bb172503fd40

JsonDb can directly save certain types.
It improves the performance that no un-necessary conversion is used.

Change-Id: I997dac8a9c60daf89cd6e2361d48931d4b00cdc4
Reviewed-by: Xizhi Zhu <xizhi.zhu@nokia.com>

src/plugins/organizer/jsondb/qorganizerjsondbconverter.cpp
@@ -1344,7 +1344,11 @@ void QOrganizerJsonDbConverter::extendedDetailToJsonDbProperty(const QOrganizerI
     if (extendedDetail.name().isEmpty() || data.isNull())
         return;
 
-    if (data.canConvert(QVariant::String)) {
+    if (data.type() < QVariant::Map) {
+        // JsonDb can directly save these types: bool, int, uint, qlonglong, qulonglong, double, QChar
+        // so no conversion is needed
+        property->setValue(data);
+    } else if (data.canConvert(QVariant::String)) {
         property->setValue(data.toString());
     } else if (data.type() == QVariant::List) {
         QVariantList variantList;
@@ -1367,7 +1371,11 @@ void QOrganizerJsonDbConverter::dataToList(const QVariant &data, QVariantList *l
         if (variant.isNull())
             continue;
 
-        if (variant.canConvert(QVariant::String)) {
+        if (variant.type() < QVariant::Map) {
+            // JsonDb can directly save these types: bool, int, uint, qlonglong, qulonglong, double, QChar
+            // so no conversion is needed
+            list->append(variant);
+        } else if (variant.canConvert(QVariant::String)) {
             list->append(variant.toString());
         } else if (variant.type() == QVariant::List) {
             dataToList(variant, list);
@@ -1388,7 +1396,11 @@ void QOrganizerJsonDbConverter::dataToMap(const QVariant &data, QVariantMap *map
     QMap<QString, QVariant>::const_iterator i = originalMap.constBegin();
     while (i != originalMap.constEnd()) {
         if (i.value().isValid() && !i.value().isNull()) {
-            if (i.value().canConvert(QVariant::String)) {
+            if (i.value().type() < QVariant::Map) {
+                // JsonDb can directly save these types: bool, int, uint, qlonglong, qulonglong, double, QChar
+                // so no conversion is needed
+                map->insert(i.key(), i.value());
+            } else if (i.value().canConvert(QVariant::String)) {
                 map->insert(i.key(), i.value().toString());
             } else if (i.value().type() == QVariant::List) {
                 QVariantList list;

commit 99aab2f44e14fe009cb43b5a364d2c46d2c0d9cd

Fix detail filter conversion in JsonDb.
It's a regression caused by commit:
3a359d450fd927b58a4172a71a57bb172503fd40

Change-Id: I4c9e667c332d018371e42965c8a303b93a9a4edf
Reviewed-by: Päivi Rajala <paivi.rajala@nokia.com>

src/plugins/organizer/jsondb/qorganizerjsondbconverter.cpp
@@ -1646,10 +1646,11 @@ bool QOrganizerJsonDbConverter::detailFilterToJsondbQuery(const QOrganizerItemFi
 
     if (isValidFilter) { // proceed with jsondb query construction, if filter is valid
         const QString equalsQueryTemplate(QStringLiteral("[?%1=\"%2\"]"));
+        const QString equalsQueryTemplate2(QStringLiteral("[?%1=%2]"));
+        const QString equalsQueryTemplate3(QStringLiteral("[?%1.%2.%3=%4]"));
         const QString containsQueryTemplate(QStringLiteral("[?%1 contains \"%2\"]"));
         const QString matchFlagQueryTemplate(QStringLiteral("[?%1%2\"]"));
         const QString matchFlagQueryTemplate2(QStringLiteral("[?%1.%2%3\"]"));
-        const QString matchFlagQueryTemplate3(QStringLiteral("[?%1.%2.%3%4\"]"));
         const QString existsQueryTemplate(QStringLiteral("[?%1 exists]"));
 
         if (QOrganizerEventTime::DefinitionName == detailType) {
@@ -1660,7 +1661,7 @@ bool QOrganizerJsonDbConverter::detailFilterToJsondbQuery(const QOrganizerItemFi
                 jsonDbQueryStr->append(equalsQueryTemplate
                     .arg(QOrganizerJsonDbStr::eventEndDateTime()).arg(df.value().toDateTime().toUTC().toString(Qt::ISODate)));
             } else if (QOrganizerEventTime::FieldAllDay == detailField) {
-                jsonDbQueryStr->append(equalsQueryTemplate
+                jsonDbQueryStr->append(equalsQueryTemplate2
                     .arg(QOrganizerJsonDbStr::eventIsAllDay()).arg(valueString));
             }
 
@@ -1672,7 +1673,8 @@ bool QOrganizerJsonDbConverter::detailFilterToJsondbQuery(const QOrganizerItemFi
                 jsonDbQueryStr->append(equalsQueryTemplate
                     .arg(QOrganizerJsonDbStr::todoDueDateTime()).arg(df.value().toDateTime().toUTC().toString(Qt::ISODate)));
             } else if (QOrganizerTodoTime::FieldAllDay == detailField) {
-                jsonDbQueryStr->append(equalsQueryTemplate.arg(QOrganizerJsonDbStr::todoIsAllDay()).arg(valueString));
+                jsonDbQueryStr->append(equalsQueryTemplate2
+                    .arg(QOrganizerJsonDbStr::todoIsAllDay()).arg(valueString));
             }
 
         } else if (QOrganizerItemDetail::TypeTodoProgress == detailType) {
@@ -1681,7 +1683,7 @@ bool QOrganizerJsonDbConverter::detailFilterToJsondbQuery(const QOrganizerItemFi
                     .arg(QOrganizerJsonDbStr::todoFinishedDateTime())
                     .arg(df.value().toDateTime().toUTC().toString(Qt::ISODate)));
             } else if (QOrganizerTodoProgress::FieldPercentageComplete == detailField) {
-                jsonDbQueryStr->append(equalsQueryTemplate
+                jsonDbQueryStr->append(equalsQueryTemplate2
                     .arg(QOrganizerJsonDbStr::todoProgressPercentage())
                     .arg(df.value().toInt()));
             } else if (QOrganizerTodoProgress::FieldStatus == detailField) {
@@ -1718,17 +1720,17 @@ bool QOrganizerJsonDbConverter::detailFilterToJsondbQuery(const QOrganizerItemFi
                     .arg(QOrganizerJsonDbStr::itemLocationLabel())
                     .arg(createMatchFlagQuery(valueString, df.matchFlags())));
             } else if (QOrganizerItemLocation::FieldLongitude ==  detailField) {
-                jsonDbQueryStr->append(matchFlagQueryTemplate3
+                jsonDbQueryStr->append(equalsQueryTemplate3
                     .arg(QOrganizerJsonDbStr::itemLocation())
                     .arg(QOrganizerJsonDbStr::itemLocationGeo())
                     .arg(QOrganizerJsonDbStr::itemLocationGeoLongitude())
-                    .arg(createMatchFlagQuery(valueString, df.matchFlags())));
+                    .arg(valueString));
             } else if (QOrganizerItemLocation::FieldLatitude ==  detailField) {
-                jsonDbQueryStr->append(matchFlagQueryTemplate3
+                jsonDbQueryStr->append(equalsQueryTemplate3
                     .arg(QOrganizerJsonDbStr::itemLocation())
                     .arg(QOrganizerJsonDbStr::itemLocationGeo())
                     .arg(QOrganizerJsonDbStr::itemLocationGeoLatitude())
-                    .arg(createMatchFlagQuery(valueString, df.matchFlags())));
+                    .arg(valueString));
             }
 
         } else if (QOrganizerItemPriority::DefinitionName == detailType
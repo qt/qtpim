qt/qtpim

commit 5a16d7827d372caeae2f4ce368e9e1c134703e55

Remove unused member variable from declarative contact model.
Change-Id: I43693dd44db9458b02e81807741ff16e4c3e8f10
Reviewed-by: Mikko Suonio <mikko.suonio@nokia.com>

src/imports/contacts/qdeclarativecontactmodel_p.h
@@ -185,10 +185,11 @@ private slots:
     void onFetchContactsRequestStateChanged(QContactAbstractRequest::State state);
 
 private:
-    QDeclarativeContactModelPrivate* d;
-    QList<QContactId> m_updatedIds;
     void checkError(const QContactAbstractRequest *request);
     int contactIndex(const QDeclarativeContact* contact);
+
+private:
+    QDeclarativeContactModelPrivate* d;
 };
 
 QTCONTACTS_END_NAMESPACE

commit f280d82435e78e48f95ab3f74f9e7f530a25ab4f

Fix a small styling issue introduced by commit 5a16d782
Change-Id: Ie95c46bbda238424bdc0ffeac419cc0b15107094
Reviewed-by: Andy Cox <andrew.cox@nokia.com>

src/imports/contacts/qdeclarativecontactmodel_p.h
@@ -185,11 +185,9 @@ private slots:
     void onFetchContactsRequestStateChanged(QContactAbstractRequest::State state);
 
 private:
+    QDeclarativeContactModelPrivate *d;
     void checkError(const QContactAbstractRequest *request);
     int contactIndex(const QDeclarativeContact* contact);
-
-private:
-    QDeclarativeContactModelPrivate* d;
 };
 
 QTCONTACTS_END_NAMESPACE
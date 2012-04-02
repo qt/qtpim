d59fc09ae98e17523fa02154c58ac797c67e4fe7
Xizhi Zhu <xizhi.zhu@nokia.com>
2012-04-02 12:06:00 +0300

    Add warning for private headers in JsonDb backend.
    Change-Id: Ife3b983c76dbb8b1539bd638dd8f217a62d8a3c7
    Reviewed-by: Xizhi Zhu <xizhi.zhu@nokia.com>

--- src/plugins/organizer/jsondb/qorganizerjsondbdatastorage.h
+++ src/plugins/organizer/jsondb/qorganizerjsondbdatastorage.h
@@ -42,6 +42,17 @@
 #ifndef QORGANIZERJSONDBDATASTORAGE_H
 #define QORGANIZERJSONDBDATASTORAGE_H
 
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API.  It exists purely as an
+// implementation detail.  This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
+
 #include <QThread>
 #include <QWaitCondition>
 #include <QList>
